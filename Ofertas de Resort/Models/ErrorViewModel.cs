18b18ed5aca7c34f2bd897209cd9239c6c6d4367
Max <Max@DESKTOP-NN525VM>
2024-09-26 23:22:02 -0400

    Agregar archivos de proyecto.

---
+++ Ofertas de Resort/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Ofertas_de_Resort.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
