7716b516cf4b2cf97cf9b0ca6efecddad37c1a63
Maxwell Tavares <maxwelltrt0709@gmail.com>
2024-10-02 00:17:10 -0400

    Destinos turísticos y arreglos del main form

---
+++ Ofertas de Resort/Controllers/DestinyController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ofertas_de_Resort.Controllers
+{
+    public class DestinyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
